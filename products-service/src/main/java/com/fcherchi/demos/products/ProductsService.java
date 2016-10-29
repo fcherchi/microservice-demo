f80d75ecfff3d24104041ebccb6af5e26df59bd5
Fernando Cherchi <fernando.cherchi@fdmediagroep.nl>
2016-10-29 17:49:12 +0200

    Working with Authorization Server in the Zuul server.

--- products-service/src/main/java/com/fcherchi/demos/products/ProductsService.java
+++ products-service/src/main/java/com/fcherchi/demos/products/ProductsService.java
@@ -5,27 +5,27 @@ import com.fcherchi.demos.products.model.Product;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 
 /**
  * Created by Fernando Cherchi on 02/10/16.
  */
 
-@SpringBootApplication
-@EnableEurekaClient
-@EnableResourceServer
+@EnableAutoConfiguration
+@Configuration
+@EnableOAuth2Sso
 @RestController
 public class ProductsService {
 
@@ -46,10 +46,9 @@ public class ProductsService {
     }
 
     @RequestMapping("/")
-    public String home() {
-        return "Hello World";
+    public String home(Principal user) {
+        return "Hello " + user.getName() + " From Products Services";
     }
-
 
 
     @RequestMapping("/{productId}")
